86bdd2f630cab050cc991f7a62038f739ce682f1
Sahil Mann <mannsahil857@gmail.com>
2026-07-21 19:59:59 -0400

    Add comments

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from "express";
-import http from "http";
+import http from "http"; // --> For creating server
 import { Server } from "socket.io";
-import path from "path";
-import { fileURLToPath } from "url";
+import path from "path"; // --> For serving static file in "public" folder
+import { fileURLToPath } from "url"; // --> Because of ES6 create __filename & __dirname
 
 // --> Because of ES6 create __filename & __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/*  
+--> Express handles HTTP requests and responses, while Socket.IO handles WebSocket connections. We create an HTTP server using the Express app and then attach Socket.IO to that server. This allows us to handle both HTTP requests and WebSocket connections in the same application. 
+*/
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); // Bridge between express & socket.io
 const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
@@ -22,6 +25,9 @@ app.use(express.static(path.join(__dirname, "public")));
 //   res.sendFile(path.join(__dirname, "index.html"));
 // });
 
+/* 
+ --> io.on("connection") is an event listener that listens for new client connections. When a client connects, it triggers the callback function, which receives a socket object representing the connection. Inside this callback, we can emit messages to the connected client and listen for messages from the client. 
+*/
 io.on("connection", (socket) => {
   console.log("A user connected.");
   // Emitting message to the client
