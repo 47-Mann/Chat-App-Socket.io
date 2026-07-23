7b442731a242b4b919e2cab751cd690a2c8ad7fe
Sahil Mann <mannsahil857@gmail.com>
2026-07-23 14:32:08 -0400

    Add the main logic

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
 const socket = io();
+
+const sendButn = document.getElementsByClassName(".send-button");
+const messageArea = document.getElementById("#messageArea");
+const input = document.querySelector("#messageInput");
+
+function addMessage(message, sender) {
+  const messageDiv = document.createElement("div");
+  messageDiv.className = "message ${sender}-message";
+  messageDiv.textContent = message;
+  messageArea.appendChild(messageDiv);
+  messageArea.scrollTop = messageArea.scrollHeight;
+}
